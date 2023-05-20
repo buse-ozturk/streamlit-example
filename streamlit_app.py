65b3f1f1a7ea0f6d103450d8642036153bc6253c
buse-ozturk <134150076+buse-ozturk@users.noreply.github.com>
2023-05-20 17:35:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,47 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
+import numpy as np
 import streamlit as st
+import datetime as dt
+import yfinance as yf
 
-"""
-# Welcome to Streamlit!
+liste={
+    "BTC":"BTC-USD",
+    "ETH":"ETH-USD",
+    "XRP":"XRP-USD,",
+    "SHIB":"SHIB-USD"
+}
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+sec=st.sidebar.selectbox("KriptoPara",liste.keys())
+ticker=liste.get(sec)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def veri(ticker,baslangic="2004-01-01",bitis=dt.datetime.today().date()):
+    df=yf.download(ticker,baslangic,bitis)
+    df=df["Close"]
+    st.line_chart(df)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+
+col1,col2,col3=st.columns(3)
+with col1:
+    son30=st.button("30GÜN")
+with col2:
+    son90 = st.button("90GÜN")
+with col3:
+    son360 = st.button("Yıl")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+bugun=dt.datetime.today().date()
+if son30:
+    baslangic=bugun-dt.timedelta(days=30)
+    bitis=bugun
+elif son90:
+    baslangic=bugun-dt.timedelta(days=90)
+    bitis=bugun
+elif son360:
+    baslangic = bugun - dt.timedelta(days=365)
+    bitis = bugun
+else:
+    baslangic="2004-01-01"
+    bitis=bugun
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+veri(ticker,baslangic,bitis)
